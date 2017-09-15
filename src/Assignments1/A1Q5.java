b6404e70ade857c14c344d073e7332bafb1765df
nagra2700 <nagra2700@HRH-IC0027343.wrdsb.ca>
2017-09-15 14:07:07 -0400

    Finished questions 1-5.

--- src/Assignments1/A1Q5.java
+++ src/Assignments1/A1Q5.java
@@ -4,6 +4,11 @@
  */
 package Assignments1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
 /**
  *
  * @author nagra2700
@@ -15,6 +20,15 @@ public class A1Q5 {
      */
     public static void main(String[] args) {
     //Creating a new city
-    City 
+    City commit=new City();
+    //Creating new robots
+    Robot IOS=new Robot(commit,0,1,Direction.WEST);
+    Robot Andriod=new Robot(commit,3,3,Direction.EAST);
+    //Creating walls
+    new Wall(commit,3,3,Direction.SOUTH);
+    new Wall(commit,3,3,Direction.EAST);
+    new Wall(commit,2,3,Direction.EAST);
+    new Wall(commit,2,3,Direction.NORTH);
+    new Wall(commit,2,3,Direction.WEST);
     }
 }
