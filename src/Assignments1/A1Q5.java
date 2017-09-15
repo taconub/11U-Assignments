4ecf5bdbdcf3d9c71c8516fcdc8471388c0a220f
nagra2700 <nagra2700@HRH-IC0027343.wrdsb.ca>
2017-09-15 14:17:51 -0400

    Finished questions 1-5.

--- src/Assignments1/A1Q5.java
+++ src/Assignments1/A1Q5.java
@@ -7,6 +7,7 @@ package Assignments1;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /**
@@ -30,5 +31,43 @@ public class A1Q5 {
     new Wall(commit,2,3,Direction.EAST);
     new Wall(commit,2,3,Direction.NORTH);
     new Wall(commit,2,3,Direction.WEST);
+    //placing Things
+    new Thing(commit,2,2);
+    new Thing(commit,1,2);
+    new Thing(commit,1,1);
+    new Thing(commit,1,0);
+    new Thing(commit,0,0);
+    IOS.setLabel("I");
+    Andriod.setLabel("A");
+    //Making Andriod trun
+    Andriod.turnLeft();
+    Andriod.turnLeft();
+    //Making robots move
+    IOS.move();
+    Andriod.move();
+    //Making IOS pick up Thing
+    IOS.pickThing();
+    //Making robots trun
+    Andriod.turnLeft();
+    Andriod.turnLeft();
+    Andriod.turnLeft();
+    
+    IOS.turnLeft();
+    //Making Robots move
+    Andriod.move();
+    IOS.move();
+    //Making robots pick up things
+    IOS.pickThing();
+    Andriod.pickThing();
+    //Making IOS turn
+    IOS.turnLeft();
+    //Making Robots move
+    Andriod.move();
+    IOS.move();
+    //Making robots pick up things
+    IOS.pickThing();
+    Andriod.pickThing();
+    //Making robots trun
+    Andriod.turnLeft();
     }
 }
