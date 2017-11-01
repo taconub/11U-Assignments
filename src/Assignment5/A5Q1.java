55e9d3a8161552cb72e93015e56fa0ce12394f06
nagra2700 <nagra2700@HRH-IC0027343.wrdsb.ca>
2017-11-01 13:40:39 -0400

    Assignment 5 completed. Sorry about the length, I tried using "or" statements and had a hard time making it work so I decided to do it this way.

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -19,30 +19,87 @@ public class A5Q1 {
         //create a scanner for input
         Scanner input = new Scanner(System.in);
         //ask the user for a word
-        System.out.println("What's the word you woud like to have translated?");
+        System.out.println("What's the word you woud like to have translated to ubbi dubbi?");
         //Getting the word
-        String word = input.nextLine();
+        String word1 = input.nextLine();
         //turning all the letters in the word into lowercase
-        word = word.toLowerCase();
-        //creating a string for translating later on
-        String transa = "";
-        //
+        word1 = word1.toLowerCase();
+        //creating a new string that is blank
+        String trans = "";
+        //creating a new string for word with spaces after it so the code works
+        String word = word1 + "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   ";
+        //when there is no vowel, search the next letter
         for (int i = 0; i < word.length(); i++) {
-            //if word is exit than stop the program
+            //if the word is exit, than end the program
             if (word.equals("exit")) {
                 break;
             }
             //if any of these vowels have been found than the word will be altered
             if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
                 //adding ub prior to the found vowel 
-                transa = transa + "ub" + word.charAt(i);
+                trans = trans + "ub" + word.charAt(i);
+                //if the next letetr is e, the word won't be changed
+                if (word.charAt(i++) == 'e') {
+                    //leaving the word as is
+                    trans = trans + word.charAt(i);
+                } else {
+                    //if the next letter is not a e, the word stays a sis
+                    trans = trans + word.charAt(i);
+                }
             } else {
-                //if there's no vowel than it will keep the word as is
-                transa = transa + word.charAt(i);
+                //if the letter isn't a vowel the word will stay as is
+                trans = trans + word.charAt(i);
             }
-
+            //if any of these vowels have been found than the word will be altered
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
+                //if the next letetr is a, the word won't be changed
+                if (word.charAt(i++) == 'a') {
+                    //leaving the word as is
+                    trans = trans + word.charAt(i);
+                } else {
+                    //if the next letter is not a a, the word stays a sis
+                    trans = trans + word.charAt(i);
+                }
+            } else {
+            }
+            //if any of these vowels have been found than the word will be altered
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
+                //if the next letetr is u, the word won't be changed
+                if (word.charAt(i++) == 'u') {
+                    //leaving the word as is
+                    trans = trans + word.charAt(i);
+                } else {
+                    //if the next letter is not a u, the word stays a sis
+                    trans = trans + word.charAt(i);
+                }
+            } else {
+            }
+            //if any of these vowels have been found than the word will be altered
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
+                //if the next letetr is o, the word won't be changed
+                if (word.charAt(i++) == 'o') {
+                    //leaving the word as is
+                    trans = trans + word.charAt(i);
+                } else {
+                    //if the next letter is not a o, the word stays a sis
+                    trans = trans + word.charAt(i);
+                }
+            } else {
+            }
+            //if any of these vowels have been found than the word will be altered
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
+                //if the next letetr is i, the word won't be changed
+                if (word.charAt(i++) == 'i') {
+                    //leaving the word as is
+                    trans = trans + word.charAt(i);
+                } else {
+                    //if the next letter is not a i, the word stays a sis
+                    trans = trans + word.charAt(i);
+                }
+            } else {
+            }
         }
         //print out the translated word
-        System.out.println(transa);
+        System.out.println(trans);
     }
 }
