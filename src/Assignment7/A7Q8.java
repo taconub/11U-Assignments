ff5dc8c06f65f330f3d16616131c0c9fdcb17335
anmoln3597 <anmoln3597@192.168.0.15>
2017-12-12 12:24:17 -0500

    Question 8 assignment 7.

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -13,35 +13,52 @@ import java.util.Scanner;
  */
 public class A7Q8 {
 
-    public double allDigitsOdd(double inty) {
-
+    public void allDigitsOdd(String inti) {
+        //creating a secondary variable
+        int inty = 0;
+        //
+        if (inti.charAt(0) == '-') {
+            inty = Integer.parseInt(inti);
+            inty = inty * -1;
+        } else {
+            inty = Integer.parseInt(inti);
+        }
+        //creating a second version of the initial integer
+        int ger = inty + 0;
+        //as long as the integer is larger than 0
         while (inty > 0) {
+            //breaking up the integer into the last digit
             inty = inty % 10;
-
+            //i f the last digit is even           
             if (inty % 2 == 0) {
+                //telling the user there is at least one even digit in the integer
                 System.out.println("There is at least one even digit in the integer.");
                 break;
-
             }
-            System.out.println(inty);
-            inty = inty / 10;
+            //using the second integer and dividing  it by ten to get rid of the last digit so another digit can be check
+            inty = ger / 10;
+            //once it is less than ten and none of the digits check were even
+            if (inty < 10) {
+                //if the final digit is odd as well
+                if (inty % 2 == 1) {
+                    //tell the user each digit is odd
+                    System.out.println("There is only odd digits in the integer.");
+                    break;
+                }
+            }
         }
-
-        return inty;
     }
 
     public static void main(String[] args) {
         // create an object to run my methods
         A7Q8 methodStarter = new A7Q8();
-
         // create a scanner
         Scanner in = new Scanner(System.in);
-
         //asking the user what the integer is 
         System.out.println("What's the integer?");
-
-        double integer = in.nextDouble();
-
+        //getting the integer
+        String integer = in.nextLine();
+        //using the method 
         methodStarter.allDigitsOdd(integer);
 
     }
